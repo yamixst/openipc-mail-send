6c56121c539ee91b927838cb680e2f0c6ecae6dc
Mikhail Yatsenko <mikhail.yatsenko@gmail.com>
2026-02-11 00:36:48 +0300

    Add build-package script and ignore artifacts
    Add build-package.sh to assemble a mail-send-openipc tar.gz package,
    including an install.sh and the expected file layout for OpenIPC.
    Update .gitignore to ignore build/ directories and generated *.tar.gz
    files

---
+++ build-package.sh
@@ -0,0 +1,102 @@
+#!/bin/sh
+#
+# Build script to create .tar.gz package for mail-send installation on OpenIPC
+#
+
+set -e
+
+PACKAGE_NAME="mail-send-openipc"
+VERSION="1.0.0"
+BUILD_DIR="build"
+PACKAGE_DIR="${BUILD_DIR}/${PACKAGE_NAME}"
+
+echo "Building ${PACKAGE_NAME} v${VERSION} package..."
+
+# Clean previous build
+rm -rf "${BUILD_DIR}"
+mkdir -p "${PACKAGE_DIR}"
+
+# Create directory structure
+mkdir -p "${PACKAGE_DIR}/usr/bin"
+mkdir -p "${PACKAGE_DIR}/etc/webui"
+mkdir -p "${PACKAGE_DIR}/var/www/cgi-bin"
+
+# Copy files
+echo "Copying files..."
+
+# mail-send binary -> /usr/bin/
+cp mail-send "${PACKAGE_DIR}/usr/bin/"
+chmod +x "${PACKAGE_DIR}/usr/bin/mail-send"
+
+# openipc/mail script -> /usr/bin/
+cp openipc/mail "${PACKAGE_DIR}/usr/bin/"
+chmod +x "${PACKAGE_DIR}/usr/bin/mail"
+
+# openipc/mail.conf.example -> /etc/webui/mail.conf
+cp openipc/mail.conf.example "${PACKAGE_DIR}/etc/webui/mail.conf"
+chmod 644 "${PACKAGE_DIR}/etc/webui/mail.conf"
+
+# openipc/ext-mail.cgi -> /var/www/cgi-bin/
+cp openipc/ext-mail.cgi "${PACKAGE_DIR}/var/www/cgi-bin/"
+chmod +x "${PACKAGE_DIR}/var/www/cgi-bin/ext-mail.cgi"
+
+# Copy post-install script
+cp openipc/add-mail-menu.sh "${PACKAGE_DIR}/"
+chmod +x "${PACKAGE_DIR}/add-mail-menu.sh"
+
+# Create install script
+cat > "${PACKAGE_DIR}/install.sh" << 'EOF'
+#!/bin/sh
+#
+# Install script for mail-send on OpenIPC
+#
+
+set -e
+
+SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
+
+echo "Installing mail-send..."
+
+# Copy files to their destinations
+cp -f "${SCRIPT_DIR}/usr/bin/mail-send" /usr/bin/
+chmod +x /usr/bin/mail-send
+
+cp -f "${SCRIPT_DIR}/usr/bin/mail" /usr/bin/
+chmod +x /usr/bin/mail
+
+# Only copy config if it doesn't exist
+if [ ! -f /etc/webui/mail.conf ]; then
+    mkdir -p /etc/webui
+    cp -f "${SCRIPT_DIR}/etc/webui/mail.conf" /etc/webui/
+    chmod 644 /etc/webui/mail.conf
+    echo "Config file installed: /etc/webui/mail.conf"
+else
+    echo "Config file already exists, skipping: /etc/webui/mail.conf"
+fi
+
+cp -f "${SCRIPT_DIR}/var/www/cgi-bin/ext-mail.cgi" /var/www/cgi-bin/
+chmod +x /var/www/cgi-bin/ext-mail.cgi
+
+echo "Adding Mail menu to web interface..."
+"${SCRIPT_DIR}/add-mail-menu.sh"
+
+echo ""
+echo "Installation complete!"
+echo "Edit /etc/webui/mail.conf to configure your mail settings."
+EOF
+chmod +x "${PACKAGE_DIR}/install.sh"
+
+# Create the tar.gz package
+echo "Creating package..."
+cd "${BUILD_DIR}"
+tar -czvf "${PACKAGE_NAME}.tar.gz" "${PACKAGE_NAME}"
+cd ..
+
+# Move package to root
+mv "${BUILD_DIR}/${PACKAGE_NAME}.tar.gz" .
+
+echo ""
+echo "Package created: ${PACKAGE_NAME}.tar.gz"
+echo ""
+echo "To install on OpenIPC, run:"
+echo "  curl -L https://YOUR_SERVER/${PACKAGE_NAME}.tar.gz | tar xz && cd ${PACKAGE_NAME} && ./install.sh"
